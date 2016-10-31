5d771046058746b21e178736101dc55bf343e8e1
Boopathi Rajaa <me@boopathi.in>
2016-11-01 00:14:03 +0100

    Add scripts

---
+++ benchmarks.md
@@ -0,0 +1,13 @@
+
+# Benchmark
+
+```
+Name              Metric 1	Metric 2
+plugin.timing.a   86		70
+plugin.timing.b   51		8
+plugin.timing.c   21		52
+plugin.timing.d   15		88
+plugin.timing.e   76		37
+plugin.timing.f   39		52
+plugin.timing.g   54		54
+```
---
+++ scripts/benchmark.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const path = require("path");
+
+const BENCHMARK_FILE = path.join(__dirname, "../benchmarks.md");
+
+function rand() {
+  return Math.floor(Math.random() * 100);
+}
+
+function generateRandomBenchmark() {
+  fs.writeFileSync(BENCHMARK_FILE,
+`
+# Benchmark
+
+\`\`\`
+Name              Metric 1\tMetric 2
+plugin.timing.a   ${rand()}\t\t${rand()}
+plugin.timing.b   ${rand()}\t\t${rand()}
+plugin.timing.c   ${rand()}\t\t${rand()}
+plugin.timing.d   ${rand()}\t\t${rand()}
+plugin.timing.e   ${rand()}\t\t${rand()}
+plugin.timing.f   ${rand()}\t\t${rand()}
+plugin.timing.g   ${rand()}\t\t${rand()}
+\`\`\`
+`
+  );
+}
+
+if (require.main === module) {
+  generateRandomBenchmark();
+}
