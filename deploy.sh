3058bb7dacc728782cbbeb56001bdd88caa3a6cc
Boopathi Rajaa <me@boopathi.in>
2016-11-01 11:57:31 +0100

    Revert changes

--- deploy.sh
+++ deploy.sh
@@ -21,22 +21,31 @@ if [ "$TRAVIS_BRANCH" != "master" ]; then
   exit 0
 fi
 
+REPO=`git config remote.origin.url`
+
+rm -rf tmp || exit 0
+mkdir tmp
 echo "Running benchmarks..."
 run_benchmark
 
-echo "Configuring..."
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
-git config credential.helper "store --file=.git/credentials"
-echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+( git clone $REPO tmp
+  cd tmp
 
-echo "Committing changes..."
-git diff
-git remote
-git add .
-git commit -m "[skip ci] Update Benchmark for ${TRAVIS_COMMIT}"
+  echo "Configuring..."
+  git config user.name "Travis CI"
+  git config user.email "$COMMIT_AUTHOR_EMAIL"
+  git config credential.helper "store --file=.git/credentials"
+  echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 
-echo "Pushing commit to master"
-git push --quiet origin master > /dev/null 2>&1
+  echo "Copying changes..."
+  cp ../benchmarks.md ./benchmarks.md
 
-echo "Done"
+  echo "Committing changes..."
+  git add .
+  git commit -m "[skip ci] Update Benchmark for ${TRAVIS_COMMIT}"
+
+  echo "Pushing commit to master..."
+  git push --quiet origin master > /dev/null 2>&1
+
+  echo "Done"
+)
